2cad2da7975af7869fbbab2620ee17512072e3d0
Noah Medoff <nmedoff123@gmail.com>
2022-10-03 14:48:12 -0700

    work on authentication redux thunk and reducers

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,21 +1,38 @@
-import { createReducer, createAction } from "@reduxjs/toolkit";
+import { createReducer, createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import { Auth } from "aws-amplify";
 
 // ACTION TYPES
-const setUser = createAction('SET_USER');
-const removeUser = createAction('REMOVE_USER')
+// const setUser = createAction('SET_USER');
+// const removeUser = createAction('REMOVE_USER')
 
-// ACTION CREATORS
-export async function signUp() {
-    try {
-        const {user} = await Auth.signUp({
-            username,
-            password,
-            autoSignIn: {
-                enabled: true,
-            }
-        })
-    } catch (error) {
-        console.log('error signing up: ', error)
+// thunks
+
+// SIGN UP
+export const signUp = (payload) => createAsyncThunk(
+    "authStore/SIGN_UP",
+    async () => { try {
+            const {user} = await Auth.signUp({
+                username,
+                password,
+                autoSignIn: {
+                    enabled: true,
+                }
+            })
+            payload = user;
+        } catch (error) {
+            console.log('error signing up: ', error)
+        }
     }
-}
+)
+
+// STATE AND REDUCER
+const userState = {}
+const authReducer = createReducer(userState, (builder) => {
+    builder
+        .addCase("authStore/SIGN_UP", (state, action) => {
+            state = { user: action.payload }
+        })
+        .addDefaultCase((state, action) => {})
+})
+
+export default authReducer;
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "./authStore";
+
+const rootReducer = {
+    authUser: authReducer
+}
+
+const store = configureStore({
+    reducer: rootReducer
+})
+
+export default store
